51edac4df15df72c0ece1c5c21d606f87f5f71b8
[sego] <s.henniart.webdev@gmail.com>
2023-06-06 12:04:02 +0200

    cours String

--- strings.py
+++ strings.py
@@ -14,40 +14,5 @@ print(text5)
 text6 = 'Foo Bar Baz'
 text6 = "Foo \"Bar\" Baz" # --> \= caractère d'échappement \\ = \ 
 
-# Exo : permuter les 2 variables en utilisant l'opérateur d'affectation &
-#       le nom des variables
-a = 123
-b = 42
-
-print(a)
-print(b)
-
-#technique 1
-a1 = (b)
-print(a1)
-b1 = (a)
-print(b1)
-a = a1
-b = b1
-
-c = b
-b = a
-a = c
-
-#technique 2
-a, b = b, a
-print(a, b)
-
-#technique 3
-a2 = 42
-b2 = 123
-
-a2 = a2 + b2
-print(a2,b2)
-b2 = a2 - b2
-print(a2,b2)
-a2 = a2 - b2
-print(a2,b2)
-
 
 
