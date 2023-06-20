eafb6e43c06338abbe1870a31250593056aa2b79
Sego <s.henniart.webdev@gmail.com>
2023-06-20 10:18:16 +0200

    exo 6.11 corrigé

--- exercices-06/exercice-06-11.py
+++ exercices-06/exercice-06-11.py
@@ -9,6 +9,20 @@ my_list = [2.71, 42, 123, 2, 3.14, 1.61]
 
 # réponse 6.11
 i=0
-for i, item in enumerate(my_list):      #pour chaque boucle effectuée & pour chaque item de my_list
-    if item == 3.14:                    # si l'item est égal à 3.14
-        print(i)                        # affiche le nombre de boucles effectuées jusqu'à l'item 3.14
+search=3.14  # pour récupérer valeur & pouvoir la réutiliser; 
+             # search=int(input()) ==> reverse lookup recherche avec le terminal
+
+for i, number in enumerate(my_list):            #pour chaque boucle effectuée & pour chaque item de my_list
+    if number == search:                        # si l'item est égal à 3.14
+        print(f'Index de 3.14 : {i}ème place')  # affiche le nombre de boucles effectuées jusqu'à l'item 3.14
+
+
+# for number in my_list:       # affiche toute la liste
+#     # @dev
+#     print(number)
+
+
+# if number == 3.14:
+#     print("Le nombre est égal à 3.14")
+# else:
+#     print("Le nombre n'est pas égal à 3.14")
