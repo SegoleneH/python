17dd1459554136b627e26903f628d9944f6b2609
Sego <s.henniart.webdev@gmail.com>
2023-06-28 20:53:26 +0200

    edit ajout fiche fonctions.js

---
+++ fonctions.js
@@ -0,0 +1,21 @@
+// DONNER DES PARAMÈTRES À UNE FONCTION
+
+function nameFunction(nameOfTargetedValue){
+    if (nameOfTargetedValue >= 42){     // <== condition
+        nameOfTargetedValue*=0.25;      // <== contenu 
+    }
+    console.log(nameOfTargetedValue);   // <== ALWAYS PUT AFTER END OF LOOP
+}
+nameFunction() // <== appeler la fonction
+
+
+// On peut définir le contenu des paramètres après la fonction
+function discount(purchase){
+    if (purchase>=1500){
+        purchase*=0.85;
+    }
+    console.log(purchase);
+}
+
+discount(1900);//discount
+discount(1200);//no discount
